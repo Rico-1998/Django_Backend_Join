3b906b6ff2a973c05fcb16bd3e8b4f4b57189164
Rico-1998 <rico.dg40@gmail.com>
2023-04-02 10:46:37 +0200

    final version

--- Kanban_API/settings.py
+++ Kanban_API/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'localhost'
+    'localhost',
+    'https://rico1998.pythonanywhere.com/'
 ]
 
 
@@ -129,7 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5502",]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5502",
+                        'https://rico1998.pythonanywhere.com']
 
 
 REST_FRAMEWORK = {
